0d52bd24a62355bfde394bc7f23960dda4fe8bae
chako-mountain <gonzaburouduanyewufu@gmail.com>
2025-04-29 09:52:42 +0900

    ESP32-uno間 構造体データ送信成功

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 
 
 
-//尾翼側で構造体を送信するコード
+// // 尾翼側で構造体を送信するコード
 // #include <Arduino.h>
 // #include "SoftwareSerial.h"
 
@@ -45,93 +45,102 @@
 
 
 
-//mainOBC側で構造体を受信するコード
-#include <Arduino.h>
-#include "SoftwareSerial.h"
+// //mainOBC側で構造体を受信するコード
+// //また、ESP32からのリクエストに応じて構造体データを送信
+// #include <Arduino.h>
+// #include "SoftwareSerial.h"
+// #include <Wire.h>
 
-struct dakakuData {
-  float rudder = 0;
-  float elevetor = 0;
-};
+// #define SLAVE_ADDRESS 0x08 
 
-struct withHight {
-  float rudder = 0;
-  float elevetor = 0;
-  float hight = 0;
-};
+// struct dakakuData {
+//   float rudder = 0;
+//   float elevetor = 0;
+// };
 
-dakakuData data;
-withHight data2;
+// struct withHight {
+//   float rudder = 0;
+//   float elevetor = 0;
+//   float hight = 0;
+// };
 
-const int pin = A0;
+// dakakuData data;
+// withHight data2;
+
+// const int pin = A0;
 
 
-SoftwareSerial mySerial(12, 11);
+// SoftwareSerial mySerial(12, 11);
 
-void setup() {
-  Serial.begin(115200);
-  mySerial.begin(9600);
+// void sendData() {
+//   Wire.write((uint8_t*)&data2, sizeof(data2));
+//   Serial.println("data sent to master"); // デバッグ用のメッセージ
+// }
 
-  pinMode(pin, INPUT);
-}
+// void setup() {
+//   Serial.begin(115200);
+//   mySerial.begin(9600);
+//   Wire.begin(SLAVE_ADDRESS); 
+//   Wire.onRequest(sendData);
 
-void loop() {
-  if (mySerial.available() >= sizeof(data)) {
-    mySerial.readBytes((uint8_t*)&data, sizeof(data));
+//   pinMode(pin, INPUT);
+// }
+
+// void loop() {
+//   if (mySerial.available() >= sizeof(data)) {
+//     mySerial.readBytes((uint8_t*)&data, sizeof(data));
     
-  }
-  data2.rudder = data.rudder;
-  data2.elevetor = data.elevetor;
+//   }
+//   data2.rudder = data.rudder;
+//   data2.elevetor = data.elevetor;
 
-  int h = 0;
-  int datasum = 0;
-  int rawdata[5];
+//   int h = 0;
+//   int datasum = 0;
+//   int rawdata[5];
 
-  for (int i = 0; i < 4; i++) {
-    rawdata[i] = 0;
-  }
+//   for (int i = 0; i < 4; i++) {
+//     rawdata[i] = 0;
+//   }
 
 
-  for (int i = 0; i < 4; i++) {
-    rawdata[i] = analogRead(pin);
+//   for (int i = 0; i < 4; i++) {
+//     rawdata[i] = analogRead(pin);
     
-    datasum = datasum + rawdata[i];
-  }
+//     datasum = datasum + rawdata[i];
+//   }
 
 
-  int rawmax = 0;
-  int rawmin = 0;
+//   int rawmax = 0;
+//   int rawmin = 0;
 
-  for (int i = 0; i < 4; i++) {
-    if (rawmax < rawdata[i]) {
-      rawmax = rawdata[i];
-    }
-  }
+//   for (int i = 0; i < 4; i++) {
+//     if (rawmax < rawdata[i]) {
+//       rawmax = rawdata[i];
+//     }
+//   }
 
-  for (int i = 0; i < 4; i++) {
-    if (rawmin > rawdata[i]) {
-      rawmin = rawdata[i];
-    }
-  }
+//   for (int i = 0; i < 4; i++) {
+//     if (rawmin > rawdata[i]) {
+//       rawmin = rawdata[i];
+//     }
+//   }
 
-  h = (datasum - rawmax - rawmin ) / 3;
-  Serial.println(h);
+//   h = (datasum - rawmax - rawmin ) / 3;
+//   Serial.println(h);
   
-  data2.hight = h;
+//   data2.hight = h;
   
-  Serial.println(millis());
-  Serial.print("rudder: ");
-  Serial.println(data.rudder);
-  Serial.print("elevetor: ");
-  Serial.println(data.elevetor);
-  Serial.print("hight: ");
-  Serial.println(data2.hight);
+//   Serial.println(millis());
+//   Serial.print("rudder: dao");
+//   Serial.println(data.rudder);
+//   Serial.print("elevetor: ");
+//   Serial.println(data.elevetor);
+//   Serial.print("hight: ");
+//   Serial.println(data2.hight);
 
-  delay(100); // 適切な受信間隔を設定
+//   delay(100); // 適切な受信間隔を設定
 
-}
-
-
+// }
 
 
 
@@ -140,8 +149,41 @@ void loop() {
 
 
 
+//ESP32側で、とりあえずUnoからのデータを受信
+// #include <Arduino.h>
+// #include <Wire.h>
+
+// struct withHight {
+//   float rudder = 0;
+//   float elevetor = 0;
+//   float hight = 0;
+// };
+
+// #define SLAVE_ADDRESS 0x08 // スレーブのアドレス
 
 
 
+// withHight maindata; // 受信データ格納用構造体
 
+// void setup() {
+//   Serial.begin(115200);
+//   Wire.begin(); // マスターとして初期化
+// }
 
+// void loop() {
+//   Wire.requestFrom(SLAVE_ADDRESS, sizeof(maindata)); // 構造体サイズ分データ要求
+
+//   if (Wire.available() >= sizeof(maindata)) { // 必要なバイト数があるか確認
+//     Wire.readBytes((uint8_t*)&maindata, sizeof(maindata)); // 構造体全体を取得
+    
+//     // 受信データを表示
+//     Serial.print("Received rudder: ");
+//     Serial.println(maindata.rudder);
+//     Serial.print("Received elevetor: ");
+//     Serial.println(maindata.elevetor);
+//     Serial.print("Received hight: ");
+//     Serial.println(maindata.hight);
+//   }
+  
+//   delay(100); // 送信頻度調整
+// }
