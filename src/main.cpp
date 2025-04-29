24b5ed2488eef7da17165978b92a176cfaf661c5
chako-mountain <gonzaburouduanyewufu@gmail.com>
2025-04-29 10:40:20 +0900

    尾翼側UART受信7セグ、I2Cデータ受信のみ

--- src/main.cpp
+++ src/main.cpp
@@ -9,35 +9,39 @@
 
 
 
+// // // 尾翼側で構造体を送信するコード
+// // #include <Arduino.h>
+// // #include "SoftwareSerial.h"
+
+// // struct dakakuData {
+// //   float rudder = 0;
+// //   float elevetor = 0;
+// // };
+
+// // dakakuData data;
+// // SoftwareSerial mySerial(12, 11);
+
+// // void setup() {
+// //   Serial.begin(115200);
+// //   mySerial.begin(9600);
+// // }
+
+// // void loop() {
+// //   data.rudder = random(0, 100);  // ランダムな値
+// //   data.elevetor = random(0, 100);
+
+// //   Serial.print(data.rudder);  // rudderの値を表示
+
+// //   mySerial.write((uint8_t*)&data, sizeof(data));
+// //   Serial.println("Data sent!");
+// //   delay(100); // 適切な送信間隔を設定
+// // }
 
 
-// // 尾翼側で構造体を送信するコード
-// #include <Arduino.h>
-// #include "SoftwareSerial.h"
 
-// struct dakakuData {
-//   float rudder = 0;
-//   float elevetor = 0;
-// };
 
-// dakakuData data;
-// SoftwareSerial mySerial(12, 11);
 
-// void setup() {
-//   Serial.begin(115200);
-//   mySerial.begin(9600);
-// }
 
-// void loop() {
-//   data.rudder = random(0, 100);  // ランダムな値
-//   data.elevetor = random(0, 100);
-
-//   Serial.print(data.rudder);  // rudderの値を表示
-
-//   mySerial.write((uint8_t*)&data, sizeof(data));
-//   Serial.println("Data sent!");
-//   delay(100); // 適切な送信間隔を設定
-// }
 
 
 
@@ -50,6 +54,23 @@
 // #include <Arduino.h>
 // #include "SoftwareSerial.h"
 // #include <Wire.h>
+
+// const int DATA = 8;
+// const int LATCH = 10;
+// const int CLOCK = 9;
+
+// byte PATTERN[] = {
+//   0b00000011, // 0
+//   0b10011111, // 1
+//   0b00100101, // 2
+//   0b00001101, // 3
+//   0b10011001, // 4
+//   0b01001001, // 5
+//   0b01000001, // 6
+//   0b00011111, // 7
+//   0b00000001, // 8
+//   0b00001001  // 9
+// };
 
 // #define SLAVE_ADDRESS 0x08 
 
@@ -84,6 +105,10 @@
 //   Wire.onRequest(sendData);
 
 //   pinMode(pin, INPUT);
+
+//   pinMode(DATA, OUTPUT);
+//   pinMode(LATCH, OUTPUT);
+//   pinMode(CLOCK, OUTPUT);
 // }
 
 // void loop() {
@@ -127,6 +152,15 @@
 
 //   h = (datasum - rawmax - rawmin ) / 3;
 //   Serial.println(h);
+
+//   int hundreds = h / 100;         // 100の位を取得
+//   int tens = (h / 10) % 10;       // 10の位を取得
+//   int units = h % 10;             // 1の位を取得
+
+//   digitalWrite(LATCH, LOW);
+//   shiftOut(DATA, CLOCK, LSBFIRST, PATTERN[hundreds]);
+//   shiftOut(DATA, CLOCK, LSBFIRST, PATTERN[tens]);  
+//   digitalWrite(LATCH, HIGH);
   
 //   data2.hight = h;
   
@@ -149,41 +183,46 @@
 
 
 
-//ESP32側で、とりあえずUnoからのデータを受信
-// #include <Arduino.h>
-// #include <Wire.h>
-
-// struct withHight {
-//   float rudder = 0;
-//   float elevetor = 0;
-//   float hight = 0;
-// };
-
-// #define SLAVE_ADDRESS 0x08 // スレーブのアドレス
 
 
 
-// withHight maindata; // 受信データ格納用構造体
 
-// void setup() {
-//   Serial.begin(115200);
-//   Wire.begin(); // マスターとして初期化
-// }
 
-// void loop() {
-//   Wire.requestFrom(SLAVE_ADDRESS, sizeof(maindata)); // 構造体サイズ分データ要求
+// ESP32側で、とりあえずUnoからのデータを受信
+#include <Arduino.h>
+#include <Wire.h>
 
-//   if (Wire.available() >= sizeof(maindata)) { // 必要なバイト数があるか確認
-//     Wire.readBytes((uint8_t*)&maindata, sizeof(maindata)); // 構造体全体を取得
+struct withHight {
+  float rudder = 0;
+  float elevetor = 0;
+  float hight = 0;
+};
+
+#define SLAVE_ADDRESS 0x08 // スレーブのアドレス
+
+
+
+withHight maindata; // 受信データ格納用構造体
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(); // マスターとして初期化
+}
+
+void loop() {
+  Wire.requestFrom(SLAVE_ADDRESS, sizeof(maindata)); // 構造体サイズ分データ要求
+
+  if (Wire.available() >= sizeof(maindata)) { // 必要なバイト数があるか確認
+    Wire.readBytes((uint8_t*)&maindata, sizeof(maindata)); // 構造体全体を取得
     
-//     // 受信データを表示
-//     Serial.print("Received rudder: ");
-//     Serial.println(maindata.rudder);
-//     Serial.print("Received elevetor: ");
-//     Serial.println(maindata.elevetor);
-//     Serial.print("Received hight: ");
-//     Serial.println(maindata.hight);
-//   }
+    // 受信データを表示
+    Serial.print("Received rudder: ");
+    Serial.println(maindata.rudder);
+    Serial.print("Received elevetor: ");
+    Serial.println(maindata.elevetor);
+    Serial.print("Received hight: ");
+    Serial.println(maindata.hight);
+  }
   
-//   delay(100); // 送信頻度調整
-// }
+  delay(100); // 送信頻度調整
+}
